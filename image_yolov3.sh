e4911c854ec68c4116c0fad9b04f09b1da8e26a1
szxwpp <szxwpp@163.com>
2018-04-24 15:27:39 +0800

    permission

--- image_yolov3.sh
+++ image_yolov3.sh
@@ -1,6 +1,6 @@
 
 
-./darknet detector test ./cfg/coco.data ./cfg/yolov3.cfg ./yolov3.weights data/dog.jpg -i 0 -thresh 0.25
+./darknet detector test ./cfg/coco.data ./cfg/yolov3.cfg ./models/yolov3.weights data/dog.jpg -i 0 -thresh 0.25 -dont_show
 
 
 
