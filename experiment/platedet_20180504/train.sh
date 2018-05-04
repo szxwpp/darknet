67342f3548f69d4507a446e9b5d81aa90a8ccecd
szxwpp <szxwpp@163.com>
2018-05-04 10:37:31 +0800

    eval plate

--- experiment/platedet_20180504/train.sh
+++ experiment/platedet_20180504/train.sh
@@ -15,5 +15,5 @@ cd ../../
 ./darknet detector train \
 ./experiment/$experiment_case/plate.data \
 ./experiment/$experiment_case/train.cfg \
-/ssd/shizhixiang/models/platedet_20180502/train_1400.weights \
--gpus 3 2>&1 | tee ./experiment/$experiment_case/train.log
+/ssd/shizhixiang/models/platedet_20180504/train_2400.weights \
+-gpus 2,3 2>&1 | tee ./experiment/$experiment_case/train.log
